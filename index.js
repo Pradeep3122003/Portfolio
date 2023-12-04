d2017deece00f20bbbdb34aea9f3d55fc23c62ab
Pradeep <102895885+Pradeep3122003@users.noreply.github.com>
2023-12-04 19:55:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+
+bt2 = document.getElementById("send");
+bt1 = document.getElementById("bt");
+
+bt1.onclick=function()
+{
+    alert("Hi😃, contact me to join my team. I'm looking for your response.")
+}
+
+bt2.onclick = function()
+{
+    n = document.getElementById("n");
+    let name = n.value;
+    alert("Hey👋.."+name+" , Thanks for contacting me. I will respond you as soon as possible.")
+}
+bt.onclick=function(){
+    
+}
+pr = document.getElementById("prof");
+pr.onclick=function(){
+    window.open("fresh1.jpg");
+}
+
+let sections = document.querySelectorAll('section');
+let links = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            links.forEach(li => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        });
+    };
+        });
+    
+};
+
+const type = new Typed('.t2',{
+    strings:['Fronted Developer', '3D editor', 'Content creator'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+});
+
+ScrollReveal({
+    reset: true ,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+
+});
+
+ScrollReveal().reveal('.tagline', { origin: 'top' });
